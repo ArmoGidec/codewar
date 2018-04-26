a930712db57432068913db3f2f17f56cf46a2137
armogidec <chudak_93@mail.ru>
2018-04-26 23:31:54 +0500

    home: advancedEvents problem

--- train/advancedEvents.js
+++ train/advancedEvents.js
@@ -1,44 +1,27 @@
 function Event() {
-	let subscribes = [];
-	
-	this.subscribe = function(...funcs) {
-		for (let func of funcs) {
-			if (typeof func === 'function')	subscribes.push(func);
-		}
-		return funcs.length;
-	};
+    let subscribes = [];
+    this.subscribe = function(...funcs) {
+        for (let func of funcs) {
+            if (typeof func === 'function') subscribes.push(func);
+        }
+        return funcs.length;
+    };
 
-	this.emit = function(...args) {
-		subscribes.forEach(func => func.call(this, ...args));
-	};
+    this.emit = function(...args) {
+        console.log(args);
+        that = this;
+        subscribes.forEach(func => {
+            func.apply(that, args);
+        });
 
-	this.unsubscribe = function(...funcs) {
-		for (let func of funcs) subscribes.splice(subscribes.lastIndexOf(func), 1);
-		this.emit();
-		return funcs.length;
-	};
+    };
 
-	return this;
+    this.unsubscribe = function(...funcs) {
+        for (let func of funcs) {
+            if ((i = subscribes.lastIndexOf(func)) !== -1) subscribes.splice(i, 1);
+        }
+        return funcs.length;
+    };
+
+    return this;
 }
-
-
-function l(arr) { arr.push('l'); }
-function o(arr) { arr.push('o'); }
-
-var e = new Event(),
-	bucket = [];
-
-e.unsubscribe(l);
-e.subscribe(l, o, l);
-e.emit(bucket);
-
-//bucket should be ['l', 'o', 'l']
-console.log(bucket, ['l', 'o', 'l']); 
-
-e.unsubscribe(o, l);
-bucket = [];
-
-e.emit(bucket); //bucket should be ['l']
-
-console.log(bucket, ['l']); 
-
