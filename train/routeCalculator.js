299dad22a54f92808fe8a4768f63b65c2bbc3d48
Гатауллин Ильдар Рустамович <P02_GataullinIR@rosstat.local>
2018-05-22 17:21:51 +0500

    work: new kata

---
+++ train/routeCalculator.js
@@ -0,0 +1,68 @@
+/**
+ * @param {string} evaluate 
+ */
+function calculate(evaluate) {
+    if (/[^0-9\.\+\-\*\$]/.test(evaluate)) return '400: Bad request';
+    if (!/[\+\-\*\$]/.test(evaluate) || evaluate[0] === '-') {
+        return parseFloat(evaluate);
+    }
+    let tree = parse(evaluate);
+    console.log(tree);
+
+    return null;
+}
+
+function parse(evaluate) {
+    let tree = new BinaryTree();
+    let sign = true;
+    let value = '';
+    for (let c of evaluate) {
+        if (/\d+/.test(c) || /-/.test(c) && sign) {
+            value += c;
+            sign = false;
+        }
+        else if (/[\+\-\*\$]/.test(c)) {
+            tree.push(value);
+            tree.push(c);
+            value = '';
+            sign = true;
+        }
+    }
+    return tree;
+}
+
+function BinaryTree() {
+    this._head = null;
+    this._count = 0;
+    return this;
+}
+
+BinaryTree.prototype.push = function(x) {
+    if (this._head === null) {
+        this._head = new BinaryTreeNode(x);
+    } else {
+        this._head = AddTo(this._head, x);
+    }
+    this._count += 1;
+};
+
+function AddTo(node, value) {
+    if (/[\+\-\*\$]/.test(value)) {
+        let h = new BinaryTreeNode(value, node);
+        return h;
+    } else {
+        let r = new BinaryTreeNode(value);
+    }
+}
+
+function BinaryTreeNode(value = null, left = null, rigth = null) {
+    this._value = value;
+    this._left = left;
+    this._rigth = rigth;
+    return this;
+}
+
+console.log(calculate('55+5'), 20);
+// console.log(calculate('5-5-5-5'), -10);
+// console.log(calculate('5*5*5*5'), 625);
+// console.log(calculate('5$5$5$5'), 0.04);
