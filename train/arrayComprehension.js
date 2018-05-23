a38f630b9bac20dbb874e779aa24954fcfaa5e82
Idar <chudak_93@live.com>
2018-05-23 23:52:23 +0500

    home: finish line of list comprehension

--- train/arrayComprehension.js
+++ train/arrayComprehension.js
@@ -4,8 +4,25 @@ function ArrayComprehension(options) {
         let res = options.generator.replace(/ /g, '').split(',').filter(val => /\d+/.test(val));
         return res;
     }
-    let a = options.generator.split(',');
-    return a;
+    let result = parse(options.generator);
+    return result;
+}
+
+/**
+ * @param {string} str 
+ */
+function parse(str) {
+    let step = str[1] === '.' ? 1 : str[2] - str[0];
+    let a = str.split(',');
+    let list = build(a[0],a[a.length - 1], step);
+    return list;
+}
+
+function build(first, compr, step) {
+    let list = [Number(first)];
+    let [start, end] = compr.match(/\d+/g).map(Number);
+
+    return list;
 }
 
 console.log(ArrayComprehension({}), []);
