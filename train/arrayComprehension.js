2d6157feb95a794a9dbb18c734543deb8d0fd975
Гатауллин Ильдар Рустамович <P02_GataullinIR@rosstat.local>
2018-05-24 09:54:15 +0500

    work: done! 1st part

--- train/arrayComprehension.js
+++ train/arrayComprehension.js
@@ -1,7 +1,7 @@
 function ArrayComprehension(options) {
     if (!options.generator) return [];
-    if (!/\.\./.test(options.generator)){
-        let res = options.generator.replace(/ /g, '').split(',').filter(val => /\d+/.test(val));
+    if (!/\.\./.test(options.generator)) {
+        let res = options.generator.replace(/ /g, '').split(',').filter(val => /\d+/.test(val)).map(Number);
         return res;
     }
     let result = parse(options.generator);
@@ -12,22 +12,39 @@ function ArrayComprehension(options) {
  * @param {string} str 
  */
 function parse(str) {
-    let step = str[1] === '.' ? 1 : str[2] - str[0];
-    let a = str.split(',');
-    let list = build(a[0],a[a.length - 1], step);
+    let numbers = str.match(/\d+/g).map(Number);
+    let step = numbers.length === 2 ? 1 : numbers[1] - numbers[0];
+    let list = build(numbers, step);
     return list;
 }
 
-function build(first, compr, step) {
-    let list = [Number(first)];
-    let [start, end] = compr.match(/\d+/g).map(Number);
-
+function build(numbers, step) {
+    let b = compare(step);
+    let list = [];
+    for (let i = numbers[0], end = numbers[numbers.length - 1]; b(i, end); i += step) {
+        list.push(i);
+    }
     return list;
+}
+
+function compare(step) {
+    if (step > 0) return (a, b) => a <= b;
+    if (step < 0) return (a, b) => a >= b;
 }
 
 console.log(ArrayComprehension({}), []);
-console.log(ArrayComprehension({generator: '    '}), []);
-console.log(ArrayComprehension({generator: '1,3..4'}), [1,3]);
-console.log(ArrayComprehension({generator: '1,2..2'}), [1,2]);
-console.log(ArrayComprehension({generator: '3,2..2'}), [3,2]);
-console.log(ArrayComprehension({generator: '5..3'}), []);
+console.log(ArrayComprehension({
+    generator: '    '
+}), []);
+console.log(ArrayComprehension({
+    generator: '1,3..4'
+}), [1, 3]);
+console.log(ArrayComprehension({
+    generator: '1,2..2'
+}), [1, 2]);
+console.log(ArrayComprehension({
+    generator: '3,2..2'
+}), [3, 2]);
+console.log(ArrayComprehension({
+    generator: '5..3'
+}), []);
